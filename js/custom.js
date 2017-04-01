919063d557f0223e1787c7766a690e19cc61e360
Andre Samolenkow <andrewsamhere@gmail.com>
2017-04-01 21:53:51 +0300

    6

--- js/custom.js
+++ js/custom.js
@@ -50,3 +50,15 @@ $(document).ready(function(){
     } // End if
   });
 })
+
+//Slide-in
+
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
